d1a0e70c82ede270574380711d2608bc1dd24181
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-10-17 11:33:47 +0900

    리액트기본02_JSX

--- FE  1주차/React_01/first-react-project/src/App.js
+++ FE  1주차/React_01/first-react-project/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import TextComponent from './component/TextComponent';
+import Section from './component/Section';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
         <TextComponent />
+        <Section />
       </header>
     </div>
   );
---
+++ FE  1주차/React_01/first-react-project/src/component/Section.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Section = () => {
+    return (
+        <div>
+            <div>이건div</div>
+            <p>이건p</p>
+            <button>이건 btn</button>
+        </div>
+    );
+};
+
+export default Section;
