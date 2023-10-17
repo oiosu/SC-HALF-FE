e16ae9e5c50429596f49aa5c36198f6d5abe705e
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-10-17 11:03:48 +0900

    리액트기본01_컴포넌트

--- FE  1주차/React_01/first-react-project/src/App.js
+++ FE  1주차/React_01/first-react-project/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import TextComponent from './component/TextComponent';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <TextComponent />
       </header>
     </div>
   );
---
+++ FE  1주차/React_01/first-react-project/src/component/TextComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TextComponent = () => {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+};
+
+export default TextComponent;
