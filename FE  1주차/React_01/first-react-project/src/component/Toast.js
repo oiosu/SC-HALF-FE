b2b358cba200ad9718e22741188cdb8e19335a1b
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-11-06 14:28:34 +0900

    모듈  시스템

--- FE  1주차/React_01/first-react-project/src/component/Toast.js
+++ FE  1주차/React_01/first-react-project/src/component/Toast.js
@@ -3,6 +3,8 @@ import "./Toast.css";
 import ToastMessage from './ToastMessage';
 import ToastButton from './ToastButton';
 
+// import 안의 세부 로직 , named export ,  export path 
+
 const Toast = ({ message, children }) => {
     return (
         <div className={`toast toast-${message.title}`}>
