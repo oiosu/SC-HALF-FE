0addfd52a0df5540bccbd78ab9683595fbbeb1dd
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-11-06 11:00:12 +0900

    CSS 수정하기

--- FE  1주차/React_01/first-react-project/src/App.js
+++ FE  1주차/React_01/first-react-project/src/App.js
@@ -12,15 +12,15 @@ function App() {
       </div>
       <div className='toast toast-warning'>
         <main className='toast__message'>
-          <header className='toast__message-title'>Success</header>
-          <p className='toast__message-text'>Right On! Your account has been updated.</p>
+          <header className='toast__message-title'>Warning</header>
+          <p className='toast__message-text'>Hmmm. Something doesn't look right.</p>
         </main>
         <button className='toast__button'>Dismiss</button>
       </div>
       <div className='toast toast-error'>
         <main className='toast__message'>
-          <header className='toast__message-title'>Success</header>
-          <p className='toast__message-text'>Right On! Your account has been updated.</p>
+          <header className='toast__message-title'>Error</header>
+          <p className='toast__message-text'>Uh oh! Something went terribly wrong!</p>
         </main>
         <button className='toast__button'>Dismiss</button>
       </div>
